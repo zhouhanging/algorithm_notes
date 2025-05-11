7a036c7840b896d0d61f1dc45f87b166f6ee1988
zhouhang <3493329756@qq.com>
2025-05-11 16:42:02 +0800

    数据树

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const root= require('./二叉树/util/initTree');
+const root= require('./二叉树/util/initNumTree');
 
 
 var levelOrderTraverse = function(root) {
---
+++ 二叉树/util/initNumTree.js
@@ -0,0 +1,20 @@
+// 构建上面的完全二叉树
+const TreeNode = require('./TreeNode');
+
+const root = new TreeNode("1");
+root.left = new TreeNode("2");
+root.right = new TreeNode("3");
+root.left.left = new TreeNode("4");
+root.left.right = new TreeNode("5");
+root.left.left.left = new TreeNode("6");
+root.left.left.right = new TreeNode("7");
+root.left.right.left = new TreeNode("8");
+root.left.right.right = new TreeNode("9");
+root.right.left = new TreeNode("10");
+root.right.right = new TreeNode("11");
+root.right.left.left = new TreeNode("12");
+root.right.left.right = new TreeNode("13");
+root.right.right.left = new TreeNode("14");
+root.right.right.right = new TreeNode("15");
+
+module.exports = root
