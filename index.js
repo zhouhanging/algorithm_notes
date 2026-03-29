abf7659cdc8cc6f9d8a00e2cc121344639a2b8e2
zhouhang <3493329756@qq.com>
2026-03-30 00:38:26 +0800

    删除项目中的所有.idea配置文件和相关的JavaScript文件

--- index.js
+++
@@ -1,8 +0,0 @@
-import root from './tree/initNumTree.js'
-import {breadthFirstSearch} from './tree/algorithm.js'
-
-
-
-let res =  breadthFirstSearch(root,'12')
-
-console.log(res)
--- tree/TreeNode.js
+++
@@ -1,7 +0,0 @@
-export default class TreeNode {
-  constructor(value) {
-    this.val = value; // 节点的值
-    this.left = null; // 左子树
-    this.right = null; // 右子树
-  }
-}
